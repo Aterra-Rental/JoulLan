04c553c19b268d401d66cc0e6d39d139fd6b4f84
vannaksomnangdev-dev <vannaksomnang.dev@gmail.com>
2026-06-03 13:41:34 +0700

    new change

--- src/Menu/Home/Home.jsx
+++ src/Menu/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
         <div className='Big-Main'>
 
-         <p>hi jm</p>
+         <p>vathana likes mazer bros very very very very very much.    </p>
 =======
        
 
