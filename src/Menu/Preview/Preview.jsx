734929e5f857f49d16bbecd8799c3358c97ed8e9
OZAI <soursokheng@Sasorai.local>
2026-06-03 14:18:34 +0700

    somnang new update

--- src/Menu/Preview/Preview.jsx
+++ src/Menu/Preview/Preview.jsx
@@ -7,17 +7,17 @@ import '../Preview/Preview.css'
 const Preview = () => {
   return (
   <div className="Big-Main">
-      <section className=" container-fluid carousel-top pt-3">
-        <div id="carouselExampleAutoplaying" className="carousel slide  " data-bs-ride="carousel">
+      <section className=" container-fluid w-100 h-50">
+        <div id="carouselExampleAutoplaying" className="carousel slide " data-bs-ride="carousel">
             <div className="carousel-inner">
               <div className="carousel-item active">
-                <img src={car1}  alt="w" className=" carousel-pic"/>
+                <img src={car1}  alt="w" className=" carousel-pic w-100 h-[40vh]  object-cover"/>
               </div>
               <div className="carousel-item">
-                <img src={car2}  alt="w" className=" carousel-pic"/>
+                <img src={car2}  alt="w" className=" carousel-pic w-100 h-[40vh]  object-cover"/>
               </div>
               <div className="carousel-item">
-                <img src={car3}  alt="w" className=" carousel-pic"/>
+                <img src={car3}  alt="w" className=" carousel-pic w-100 h-[40vh]  object-cover"/>
               </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
